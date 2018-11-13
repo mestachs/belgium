ad683d6d1ed7b0c9d7af0ad3f7588a9cc384e43c
mestachs <mestachs>
2018-11-13 22:13:17 +0100

    Add content from wikipedia

--- src/components/CountryMap.js
+++ src/components/CountryMap.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Map, TileLayer, GeoJSON } from "react-leaflet";
 import Leaflet from "leaflet";
 import "leaflet/dist/leaflet.css";
-
+import ReactMarkdown from "react-markdown";
 import ZoneCard from "./ZoneCard";
 import Credits from "./Credits";
 import provinces from "./Provinces";
@@ -13,12 +13,27 @@ import slugify from "slugify";
 Leaflet.Icon.Default.imagePath =
   "//cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.4/images/";
 
+const wikiToMd = wiki => {
+  wiki = wiki.replace("[pas clair]", "");
+  wiki = wiki.replace("[r\u00e9f. n\u00e9cessaire]", "");
+  wiki = wiki.replace("[\u00e9vasif]", "");
+  wiki = wiki.replace(/=====\s*(.*)\s*=====/g, "#### $1\n\n");
+  wiki = wiki.replace(/====\s*(.*)\s*====/g, "### $1\n\n");
+  wiki = wiki.replace(/===\s*(.*)\s*===/g, "## $1");
+  wiki = wiki.replace(/==\s*(.*)\s*==/g, "# $1");
+
+  wiki = wiki.replace(/\[([^\s]*)\s([^\]]*)\]/g, "[$2]($1)");
+  wiki = wiki.replace(/\*\*/g, "    *");
+  return wiki;
+};
+
 const regions = [
   {
     name: { fr: "Région Wallonne", nl: "Waalse Gewest" },
     armories:
       "https://upload.wikimedia.org/wikipedia/commons/4/42/Flag_of_Wallonia.svg",
-    provinces: ["Brabant wallon", "Hainaut", "Liège", "Luxembourg", "Namur"]
+    provinces: ["Brabant wallon", "Hainaut", "Liège", "Luxembourg", "Namur"],
+    wikipedia: "Région_wallonne"
   },
   {
     name: { fr: "Région Flamande", nl: "Vlaams Gewest" },
@@ -30,7 +45,8 @@ const regions = [
       "Flandre occidentale",
       "Flandre orientale",
       "Limbourg"
-    ]
+    ],
+    wikipedia: "Région_flamande"
   },
   {
     name: {
@@ -39,7 +55,8 @@ const regions = [
     },
     armories:
       "https://upload.wikimedia.org/wikipedia/commons/b/bc/Flag_of_the_Brussels-Capital_Region.svg",
-    provinces: ["Bruxelles capitale"]
+    provinces: ["Bruxelles capitale"],
+    wikipedia: "Région_de_Bruxelles-Capitale"
   }
 ];
 
@@ -124,6 +141,20 @@ class CountryMap extends React.Component {
           if (feature.properties.slug === selectedZoneSlug) {
             selectedFeature = feature;
           }
+          if (zone.wikipedia) {
+            fetch(
+              "https://fr.wikipedia.org/w/api.php?origin=*&format=json&action=query&prop=extracts&explaintext&redirects=1&titles=" +
+                zone.wikipedia
+            )
+              .then(res => res.json())
+              .then(json => {
+                const pageKey = Object.keys(json.query.pages)[0];
+                const article = wikiToMd(json.query.pages[pageKey].extract);
+                feature.properties.article =
+                  "# " + zone.name.fr + "\n\n" + article;
+                this.setState({ demo: pageKey });
+              });
+          }
         });
 
         if (selectedFeature === undefined) {
@@ -185,6 +216,40 @@ class CountryMap extends React.Component {
           this.state.selectedFeature.properties.zone && (
             <ZoneCard zone={this.state.selectedFeature.properties.zone} />
           )}
+        <div className="wikipedia">
+          {this.state.selectedFeature && (
+            <div>
+              <a href="http://creativecommons.org/licenses/by-sa/3.0/deed.fr">
+                Contenu soumis à la licence CC-BY-SA
+              </a>
+              . Source : Article{" "}
+              <em>
+                <a
+                  href={
+                    "http://fr.wikipedia.org/wiki/" +
+                    this.state.selectedFeature.properties.zone.wikipedia
+                  }
+                >
+                  {this.state.selectedFeature.properties.zone.wikipedia}
+                </a>
+              </em>{" "}
+              de <a href="http://fr.wikipedia.org/">Wikipédia en français</a> (
+              <a
+                href={
+                  "http://fr.wikipedia.org/w/index.php?title=" +
+                  this.state.selectedFeature.properties.zone.wikipedia +
+                  "&action=history"
+                }
+              >
+                auteurs
+              </a>
+              )
+              <ReactMarkdown
+                source={this.state.selectedFeature.properties.article}
+              />
+            </div>
+          )}
+        </div>
         <Credits />
       </div>
     );
--- src/components/Provinces.js
+++ src/components/Provinces.js
@@ -4,77 +4,88 @@ const provinces = [
     name: { fr: "Bruxelles capitale", nl: "Brussel Hoofstadt" },
     chiefTown: { fr: "Bruxelles", nl: "Brussel" },
     armories:
-      "https://upload.wikimedia.org/wikipedia/commons/7/7a/Blason_de_Bruxelles-Capitale.svg"
+      "https://upload.wikimedia.org/wikipedia/commons/7/7a/Blason_de_Bruxelles-Capitale.svg",
+    wikipedia: "Région_de_Bruxelles-Capitale"
   },
   {
     id: 2,
     name: { fr: "Anvers", nl: "Antwerpen" },
     chiefTown: { nl: "Antwerpen", fr: "Anvers" },
     armories:
-      "https://upload.wikimedia.org/wikipedia/commons/0/09/Wapen_van_de_provincie_Antwerpen.svg"
+      "https://upload.wikimedia.org/wikipedia/commons/0/09/Wapen_van_de_provincie_Antwerpen.svg",
+    wikipedia: "Province_d%27Anvers"
   },
   {
     id: 4,
     name: { fr: "Flandre Orientale", nl: "Oost-Vlaanderen" },
     chiefTown: { nl: "Gent", fr: "Gand" },
     armories:
-      "https://upload.wikimedia.org/wikipedia/commons/5/51/Wapen_van_Oost-Vlaanderen.svg"
+      "https://upload.wikimedia.org/wikipedia/commons/5/51/Wapen_van_Oost-Vlaanderen.svg",
+    wikipedia: "Province_de_Flandre-Orientale"
   },
   {
     id: 3,
     name: { fr: "Limbourg", nl: "Limburg" },
     chiefTown: { fr: "Hasselt", nl: "Hasselt" },
     armories:
-      "https://upload.wikimedia.org/wikipedia/commons/c/cc/Wapen_van_Limburg_%28Belgi%C3%AB%29.svg"
+      "https://upload.wikimedia.org/wikipedia/commons/c/cc/Wapen_van_Limburg_%28Belgi%C3%AB%29.svg",
+    wikipedia: "Province_de_Limbourg_(Belgique)"
   },
   {
     id: 6,
     name: { fr: "Flandre Occidentale", nl: "West-Vlaanderen" },
     chiefTown: { fr: "Bruges", nl: "Brugge" },
     armories:
-      "https://upload.wikimedia.org/wikipedia/commons/a/a3/Wapen_van_West-Vlaanderen.svg"
+      "https://upload.wikimedia.org/wikipedia/commons/a/a3/Wapen_van_West-Vlaanderen.svg",
+    wikipedia: "Province_de_Flandre-Occidentale"
   },
   {
     id: 7,
     name: { fr: "Brabant Wallon", nl: "Waals-Brabant" },
     chiefTown: { fr: "Wavre", nl: "Waver" },
     armories:
-      "https://upload.wikimedia.org/wikipedia/commons/1/1c/Wapen_van_Waals-Brabant.svg"
+      "https://upload.wikimedia.org/wikipedia/commons/1/1c/Wapen_van_Waals-Brabant.svg",
+    wikipedia: "Province_du_Brabant_wallon"
   },
   {
     id: 8,
     name: { fr: "Hainaut", nl: "Henegouwen" },
     chiefTown: { fr: "Mons", nl: "Bergen" },
     armories:
-      "https://upload.wikimedia.org/wikipedia/commons/c/c9/H%C3%A9raldique_Province_BE_Hainaut_crown.svg"
+      "https://upload.wikimedia.org/wikipedia/commons/c/c9/H%C3%A9raldique_Province_BE_Hainaut_crown.svg",
+    wikipedia: "Province_de_Hainaut"
   },
   {
     id: 9,
     name: { fr: "Liège", nl: "Luik" },
     chiefTown: { fr: "Liège", nl: "Luik" },
     armories:
-      "https://upload.wikimedia.org/wikipedia/commons/e/e5/Blason_liege_prov_crown.svg"
+      "https://upload.wikimedia.org/wikipedia/commons/e/e5/Blason_liege_prov_crown.svg",
+    wikipedia: "Province_de_Liège"
   },
   {
     id: 12,
     name: { fr: "Namur", nl: "Namen" },
     chiefTown: { fr: "Namur", nl: "Namen" },
     armories:
-      "https://upload.wikimedia.org/wikipedia/commons/2/2e/Blason_namur_prov_crown.svg"
+      "https://upload.wikimedia.org/wikipedia/commons/2/2e/Blason_namur_prov_crown.svg",
+    wikipedia: "Province_de_Namur"
   },
   {
     id: 11,
     name: { fr: "Luxembourg", nl: "Luxemburg" },
     chiefTown: { fr: "Arlon", nl: "Aarlen" },
     armories:
-      "https://upload.wikimedia.org/wikipedia/commons/e/ed/Coat_of_arms_of_the_Province_of_Luxembourg.svg"
+      "https://upload.wikimedia.org/wikipedia/commons/e/ed/Coat_of_arms_of_the_Province_of_Luxembourg.svg",
+    wikipedia: "Province_de_Luxembourg"
   },
   {
     id: 5,
     name: { fr: "Brabant Flamand", nl: "Vlaams-Brabant" },
     chiefTown: { nl: "Leuven", fr: "Louvain" },
     armories:
-      "https://upload.wikimedia.org/wikipedia/commons/c/ca/Wapen_van_Vlaams-Brabant.svg"
+      "https://upload.wikimedia.org/wikipedia/commons/c/ca/Wapen_van_Vlaams-Brabant.svg",
+    wikipedia: "Province_du_Brabant_flamand"
   }
 ];
 
