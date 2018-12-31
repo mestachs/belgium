25fafaac1517f5042c0439020d007db7b91b58c9
mestachs <mestachs>
2019-01-01 00:01:52 +0100

    Add onair some rtbf and french radios

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ Belgium provinces, inspired by my daughter homework.
 First attempt at using Leaflet
 
 deployed as github-pages : https://mestachs.github.io/belgium/
+
+
+api used : 
+ - http://np.maradio.be/qp/v3/stations.json
+ - http://np.maradio.be/qp/v3/events?rpId=8
+ 
--- src/components/Radios.js
+++ src/components/Radios.js
@@ -4,11 +4,135 @@ import { withRouter } from "react-router-dom";
 
 import radios from "./radios.json";
 
-console.log(JSON.stringify(radios));
-
+const humanize = str => {
+  if (str === undefined) {
+    return "";
+  }
+  const lower = str.toLowerCase();
+  return lower.charAt(0).toUpperCase() + lower.substr(1);
+};
 radios.forEach(element => {
   element.slug = slugify(element.name, { lower: true });
 });
+
+const SelectedRadio = props => {
+  const { selectedRadio, playingNow, playStatus, togglePlay } = props;
+  let classNames = "playPause";
+  if (playStatus !== "playing") {
+    classNames = "playPause paused";
+  }
+  return (
+    <React.Fragment>
+      <div
+        style={{
+          paddingTop: "20px",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          minHeight: "200px",
+          flexWrap: "wrap",
+          width: "80%",
+          margin: "auto"
+        }}
+      >
+        <button className={classNames} onClick={togglePlay} />
+        <img
+          src={selectedRadio.logo}
+          style={{
+            margin: "10px",
+            maxWidth: "150px",
+            maxHeigh: "150px",
+            height: "auto",
+            backgroundColor: selectedRadio.backgroundColor
+              ? selectedRadio.backgroundColor
+              : ""
+          }}
+          alt={selectedRadio.name}
+          title={selectedRadio.name}
+        />
+        <div style={{ margin: "10px" }}>
+          <p>
+            <a
+              href={selectedRadio.site}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {selectedRadio.name}
+            </a>
+          </p>
+          <p>Streaming: {selectedRadio.listen.high}</p>
+          {playingNow && (
+            <div>
+              <div
+                style={{
+                  float: "left",
+                  height: "60px",
+                  marginRight: "5px",
+                  paddingTop: "5px"
+                }}
+              >
+                <img
+                  src={playingNow.imageUrl}
+                  width="60px"
+                  alt={playingNow.name}
+                  title={playingNow.name}
+                />
+              </div>
+              <div
+                style={{
+                  float: "left",
+                  marginTop: "10px",
+                  height: "40px"
+                }}
+              >
+                <i className="fab fa-itunes-note" />
+                &nbsp; &nbsp;
+                <b>{playingNow.name}</b>
+                <br />
+                &nbsp;&nbsp;<i>{playingNow.artistName}</i>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+      <hr />
+      <br />
+      <br />
+    </React.Fragment>
+  );
+};
+
+const RadioCard = props => {
+  const radio = props.radio;
+  return (
+    <div
+      key={radio.slug}
+      className="radio-thumbnail top-img"
+      title={radio.name}
+    >
+      <img
+        src={radio.logo}
+        style={{
+          maxWidth: "150px",
+          maxHeigh: "150px",
+          height: "auto",
+          width: "120px",
+          backgroundColor: radio.backgroundColor ? radio.backgroundColor : ""
+        }}
+        alt={radio.name}
+        title={radio.name}
+      />
+      <div
+        className="overlay"
+        onClick={() => {
+          this.play(radio);
+        }}
+      >
+        <i className="fa fa-play" />
+      </div>
+    </div>
+  );
+};
 
 class Radios extends Component {
   constructor(props) {
@@ -39,7 +163,7 @@ class Radios extends Component {
 
   play(radio, slug) {
     this.props.history.push("/belgium/radios/" + radio.slug);
-
+    this.setState({ playingNow: undefined });
     if (this.audio) {
       this.audio.pause();
     }
@@ -53,13 +177,33 @@ class Radios extends Component {
       .catch(() => {
         this.setState({ playStatus: "paused" });
       });
+
+    if (radio.onair) {
+      var proxyUrl = "https://cors-anywhere.herokuapp.com/";
+
+      fetch(proxyUrl + radio.onair)
+        .then(response => response.json())
+        .then(response => {
+          let now = response.results.now;
+          if (now.type === "SI" && response.results.previous) {
+            now =
+              response.results.previous[response.results.previous.length - 1];
+          }
+          if (now.type !== "SI") {
+            this.setState({
+              playingNow: {
+                imageUrl: now.imageUrl,
+                name: humanize(now.name || now.programmeName),
+                artistName: humanize(now.artistName)
+              }
+            });
+          }
+        });
+    }
   }
   render() {
-    const { selectedRadio, playStatus } = this.state;
-    let classNames = "playPause";
-    if (playStatus !== "playing") {
-      classNames = "playPause paused";
-    }
+    const { selectedRadio, playStatus, playingNow } = this.state;
+
     return (
       <React.Fragment>
         <link
@@ -73,50 +217,12 @@ class Radios extends Component {
         >
           {!selectedRadio && <p>Click to play</p>}
           {selectedRadio && (
-            <React.Fragment>
-              <div
-                style={{
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "center",
-                  height: "200px",
-                  flexWrap: "wrap",
-                  width: "80%",
-                  margin: "auto"
-                }}
-              >
-                <button className={classNames} onClick={this.togglePlay} />
-                <img
-                  src={selectedRadio.logo}
-                  style={{
-                    margin: "10px",
-                    maxWidth: "150px",
-                    maxHeigh: "150px",
-                    height: "auto",
-                    backgroundColor: selectedRadio.backgroundColor
-                      ? selectedRadio.backgroundColor
-                      : ""
-                  }}
-                  alt={selectedRadio.name}
-                  title={selectedRadio.name}
-                />
-                <div style={{ margin: "10px" }}>
-                  <p>
-                    <a
-                      href={selectedRadio.site}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      {selectedRadio.name}
-                    </a>
-                  </p>
-                  <p>Streaming : {selectedRadio.listen.high}</p>
-                </div>
-              </div>
-              <hr />
-              <br />
-              <br />
-            </React.Fragment>
+            <SelectedRadio
+              selectedRadio={selectedRadio}
+              playingNow={playingNow}
+              playStatus={playStatus}
+              togglePlay={this.togglePlay}
+            />
           )}
 
           <div
@@ -130,33 +236,7 @@ class Radios extends Component {
             }}
           >
             {radios.map(radio => (
-              <div
-                key={radio.slug}
-                className="radio-thumbnail top-img"
-                title={radio.name}
-              >
-                <img
-                  src={radio.logo}
-                  style={{
-                    maxWidth: "150px",
-                    maxHeigh: "150px",
-                    width: "150px",
-                    backgroundColor: radio.backgroundColor
-                      ? radio.backgroundColor
-                      : ""
-                  }}
-                  alt={radio.name}
-                  title={radio.name}
-                />
-                <div
-                  className="overlay"
-                  onClick={() => {
-                    this.play(radio);
-                  }}
-                >
-                  <i className="fa fa-play" />
-                </div>
-              </div>
+              <RadioCard radio={radio} />
             ))}
           </div>
           <br />
